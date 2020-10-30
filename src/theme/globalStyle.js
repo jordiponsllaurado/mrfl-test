5f6e8f60b89ec7318d5b352a7519ed7c6ea492a8
Jordi <jpons@ohpen.com>
2020-10-30 18:40:30 +0100

    Adds styled components

---
+++ src/theme/globalStyle.js
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  html{
+    box-sizing: border-box;
+    font-size: 10px;
+  }
+
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+
+  body {
+    padding: 0;
+    margin: 0;
+    font-size: 1.5rem;
+    line-height: 2;
+  }
+
+  *:focus{
+    outline: none;
+  }
+`;
+
+export default GlobalStyle;
