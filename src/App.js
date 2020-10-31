f758503722d843909a3d371d9eaa3feaf22aea56
Jordi <jpons@ohpen.com>
2020-10-31 10:42:23 +0100

    Creates main page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import Article from './components/Article';
+import Spinner from './components/Spinner/Spinner';
+import Topbar from './components/topbar/Topbar';
 
 function App() {
+  const [articles, setArticles] = useState(null);
+  const [customizations, setCustomizations] = useState({
+    logo: {
+      image: 'https://www.marfeel.com/public/images/landing/header/logo-marfeel.svg',
+      size: '40px'
+    },
+    background: {
+      color: 'white'
+    },
+    burgerMenu: {
+      color: 'orange'
+    }
+  });
+  const [sections, setSections] = useState(null);
+  const [sectionActive, setSectionActive] = useState(null);
+
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_API_URL}/articles`)
+      .then(response => response.json())
+      .then(data => {
+        setArticles(data);
+      });
+  }, []);
+
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_API_URL}/customizations`)
+      .then(response => response.json())
+      .then(data => {
+        setCustomizations(data);
+      });
+  }, []);
+
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_API_URL}/sections`)
+      .then(response => response.json())
+      .then(data => {
+        setSectionActive(data[0]);
+        setSections(data);
+      });
+  }, []);
+
+  const handleChangeSection = section => {
+    setSectionActive(section);
+  };
+
+  if (!articles || !sections) {
+    return <Spinner />;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Topbar
+        customizations={customizations}
+        sections={sections}
+        sectionActive={sectionActive}
+        onChangeSection={handleChangeSection}
+      />
+      <div className="content">
+        {articles && articles.map(article => <Article key={article.id} {...article} />)}
+      </div>
     </div>
   );
 }
